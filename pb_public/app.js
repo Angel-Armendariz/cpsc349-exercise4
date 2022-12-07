7c0e38aabd38b145c250fb1a3a784890b18f0fe6
Angel Armendariz <95111582+Angel-Armendariz@users.noreply.github.com>
2022-12-06 17:47:17 -0800

    finished the lab

--- pb_public/app.js
+++ pb_public/app.js
@@ -1,8 +1,5 @@
-import PocketBase from "https://unpkg.com/pocketbase@0.8.1/dist/pocketbase.umd.js";
-global.EventSource = require("eventsource");
-require("cross-fetch/polyfill");
-const USERNAME = "ADMIN_EMAIL";
-const PASSWORD = "ADMIN_PASSWORD";
+const USERNAME = "angelarmendariz@csu.fullerton.edu";
+const PASSWORD = "Xb44s5t6!!!!";
 const pb = new PocketBase("http://127.0.0.1:8090");
 const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD);
 console.log(authData);
--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,5 @@
-// Using ES modules (default)
-import PocketBase from 'https://unpkg.com/pocketbase@0.8.1/dist/pocketbase.umd.js'
-// OR if you are using CommonJS modules
-//const PocketBase = require('pocketbase/cjs')
-// npm install eventsource --save
-global.EventSource = require('eventsource');
-// npm install cross-fetch --save
-require('cross-fetch/polyfill');
-const USERNAME = 'ADMIN_EMAIL'
-const PASSWORD = 'ADMIN_PASSWORD'
+const USERNAME = 'angelarmendariz@csu.fullerton.edu'
+const PASSWORD = 'Xb44s5t6!!!!'
 
 const pb = new PocketBase('http://127.0.0.1:8090')
 
